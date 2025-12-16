b3ed5ad2ba9d19222834c5d532d4b7f10747dd91
Mohit Rohda <163033754+mohitrohda@users.noreply.github.com>
2025-12-16 22:59:30 +0530

    Update app.py

--- flask_app/app.py
+++ flask_app/app.py
@@ -79,7 +79,7 @@ def load_model(model_path, vectorizer_path):
 
 
 # Initialize the model and vectorizer
-model, vectorizer = load_model("./lgbm_model.pkl", "./tfidf_vectorizer.pkl")  
+model, vectorizer = load_model("lgbm_model.pkl", "tfidf_vectorizer.pkl")  
 
 # Initialize the model and vectorizer
 # model, vectorizer = load_model_and_vectorizer("yt_chrome_plugin_model", "1", "./tfidf_vectorizer.pkl")  #
